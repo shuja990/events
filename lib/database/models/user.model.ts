9cd42d6d84042445a0238a7c0ef511d7995a4dbb
Shuja Ali <shujaali1234@gmail.com>
2024-07-20 05:37:20 +0500

    Initial commit

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,10 +3,10 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
-  photo: { type: String, required: true },
+  username: { type: String,  unique: true },
+  firstName: { type: String, },
+  lastName: {type: String, },
+  photo: { type: String, },
 })
 
 const User = models.User || model('User', UserSchema);
