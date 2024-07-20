07f62dd0a77a4b79c0f4fcf24a9475e3e4f7be20
Shuja Ali <shujaali1234@gmail.com>
2024-07-20 05:50:35 +0500

    Bug fixes

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,12 +3,12 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String,  unique: true },
-  firstName: { type: String, },
-  lastName: {type: String, },
-  photo: { type: String, },
-})
+  username: { type: String, unique: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  photo: { type: String },
+});
 
-const User = models.User || model('User', UserSchema);
+const User = models.User || model("User", UserSchema);
 
 export default User;
