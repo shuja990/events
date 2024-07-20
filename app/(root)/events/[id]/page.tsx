6b1981a83579146a18805b542cad6f343d9875e3
Shuja Ali <shujaali1234@gmail.com>
2024-07-20 05:55:42 +0500

    Bug fixes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -42,7 +42,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
 
               <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
                 by{' '}
-                <span className="text-primary-500">{event.organizer.firstName} {event.organizer.lastName}</span>
+                <span className="text-primary-500">{event?.organizer?.firstName} {event?.organizer?.lastName}</span>
               </p>
             </div>
           </div>
